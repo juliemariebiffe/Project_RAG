e27e8e6defc9df791386fde7012e6ffef5f4717f
juliemariebiffe <166720020+juliemariebiffe@users.noreply.github.com>
2025-06-23 10:44:19 +0200

    Add files via upload

--- rag/my_langchain.py
+++ rag/my_langchain.py
@@ -1,42 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## Mise au point de processus de RAG
-# 
-# ### Installation
-
-# In[ ]:
-
-
-#pip install langchain langchain-openai langchain_community pymupdf yaml
-#pip install langchain==0.3.25 langchain-openai==0.1.7 langchain-community==0.0.36 pymupdf pyyaml
-
-
-
-# ### Imports
-
-# In[1]:
-
-
-import os
 import yaml
+
+from datetime import datetime
 
 from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.vectorstores import InMemoryVectorStore
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_core.documents import Document
+
+from langchain_openai import AzureOpenAIEmbeddings
 from langchain_openai import AzureChatOpenAI
 
 
-
-#from langchain.chains import RetrievalQA
-#from langchain.llms import OpenAI
-
-
-# ### Configuration
-
-# In[2]:
+CHUNK_SIZE = 1_000
+CHUNK_OVERLAP = 200
 
 
 def read_config(file_path):
@@ -48,36 +25,17 @@ def read_config(file_path):
             print(f"Error reading YAML file: {e}")
             return None
 
-config = read_config("C:/Users/Julie-Marie Biffe/Project_RAG/secrets/config.yaml")
+config = read_config("secrets/config.yaml")
 
 
-# ### Initialisation
-
-# In[3]:
-
-
-from langchain_openai import AzureOpenAIEmbeddings
-
-embeddings = AzureOpenAIEmbeddings(
+embedder = AzureOpenAIEmbeddings(
     azure_endpoint=config["embedding"]["azure_endpoint"],
     azure_deployment=config["embedding"]["azure_deployment"],
     openai_api_version=config["embedding"]["azure_api_version"],
     api_key=config["embedding"]["azure_api_key"]
 )
 
-
-# In[4]:
-
-
-from langchain_core.vectorstores import InMemoryVectorStore
-
-vector_store = InMemoryVectorStore(embeddings)
-
-
-# In[5]:
-
-
-from langchain_openai import AzureChatOpenAI
+vector_store = InMemoryVectorStore(embedder)
 
 llm = AzureChatOpenAI(
     azure_endpoint=config["chat"]["azure_endpoint"],
@@ -87,147 +45,9 @@ llm = AzureChatOpenAI(
 )
 
 
-# ### Extraction
-
-# In[6]:
-
-
-file_path = "C:/Users/Julie-Marie Biffe/OneDrive/Documents/mag 3/Mignot/B4LFlaparureguydemaupassant.pdf"
-loader = PyMuPDFLoader(file_path)
-
-
-# In[7]:
-
-
-docs = loader.load()
-docs[0]
-
-
-# In[8]:
-
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-all_splits = text_splitter.split_documents(docs)
-
-
-# In[9]:
-
-
-len(all_splits)
-
-
-# ### Indexation
-
-# In[10]:
-
-
-_ = vector_store.add_documents(documents=all_splits)
-
-
-# ### Interrogation
-
-# In[11]:
-
-
-def retrieve(store, question: str):
-    retrieved_docs = store.similarity_search(question)
-    return retrieved_docs
-
-
-# In[12]:
-
-
-# from langchain import hub
-
-# print(hub.pull("rlm/rag-prompt").messages[0].prompt.template)
-
-
-# In[13]:
-
-
-def build_qa_messages(question: str, context: str) -> list[str]:
-    messages = [
-    (
-        "system",
-        "You are an assistant for question-answering tasks.",
-    ),
-    (
-        "system",
-        """Use the following pieces of retrieved context to answer the question.
-        If you don't know the answer, just say that you don't know.
-        Use three sentences maximum and keep the answer concise.
-        {}""".format(context),
-    ),
-    (  
-        "user",
-        question
-    ),]
-    return messages
-
-
-# In[14]:
-
-
-question = "comment s'appelle l'amie de madame Loisel"
-docs_content = "\n\n".join(doc.page_content for doc in retrieve(vector_store, question))
-messages = build_qa_messages(question, docs_content)
-
-
-# In[15]:
-
-
-response = llm.invoke(messages)
-
-
-# In[16]:
-
-
-print(response.content)
-
-
-# ### Application à un autre fichier
-
-# In[17]:
-
-
-file_path = "C:/Users/Julie-Marie Biffe/OneDrive/Documents/mag 3/Mignot/Anomaly_Detection_The_Mathematization_of.pdf"
-loader = PyMuPDFLoader(file_path)
-docs = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-all_splits = text_splitter.split_documents(docs)
-new_vector_store = InMemoryVectorStore(embeddings)
-_ = new_vector_store.add_documents(documents=all_splits)
-question = "liste les ouvrages mentionnés dans le document"
-docs_content = "\n\n".join(doc.page_content for doc in retrieve(new_vector_store, question))
-messages = build_qa_messages(question, docs_content)
-response = llm.invoke(messages)
-print(response.content)
-
-
-# In[18]:
-
-
-question = "liste les auteurs mentionnés dans le document"
-docs_content = "\n\n".join(doc.page_content for doc in retrieve(new_vector_store, question))
-messages = build_qa_messages(question, docs_content)
-response = llm.invoke(messages)
-print(response.content)
-
-
-# In[19]:
-
-
-question = "liste les organisations mentionnées dans le document"
-docs_content = "\n\n".join(doc.page_content for doc in retrieve(new_vector_store, question))
-messages = build_qa_messages(question, docs_content)
-response = llm.invoke(messages)
-print(response.content)
-
-
-# In[24]:
-
-
 def get_meta_doc(extract: str) -> str:
+    """Generate a synthetic metadata description of the content.
+    """
     messages = [
     (
         "system",
@@ -254,28 +74,133 @@ def get_meta_doc(extract: str) -> str:
     return response.content
 
 
-# In[25]:
+def store_pdf_file(file_path: str, doc_name: str, use_meta_doc: bool=True):
+    """Store a pdf file in the vector store.
+
+    Args:
+        file_path (str): file path to the PDF file
+    """
+    loader = PyMuPDFLoader(file_path)
+    docs = loader.load()
+    # TODO: make a constant of chunk_size and chunk_overlap
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE,
+                                                   chunk_overlap=CHUNK_OVERLAP)
+    all_splits = text_splitter.split_documents(docs)
+    for split in all_splits:
+        split.metadata = {
+            'document_name': doc_name,
+            'insert_date': datetime.now()
+            }
+    if use_meta_doc:
+        extract = '\n\n'.join([split.page_content for split in all_splits[:min(10, len(all_splits))]])
+        meta_doc = Document(page_content=get_meta_doc(extract),
+                            metadata={
+                                'document_name': doc_name,
+                                'insert_date': datetime.now()
+                                })
+        all_splits.append(meta_doc)
+    _ = vector_store.add_documents(documents=all_splits)
+    return
 
 
-from langchain_community.document_loaders import PyMuPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-file_path = "C:/Users/Julie-Marie Biffe/OneDrive/Documents/mag 3/Mignot/article_nature.pdf"
-loader = PyMuPDFLoader(file_path)
-docs = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-all_splits = text_splitter.split_documents(docs)
-extract = '\n\n'.join([split.page_content for split in all_splits[:min(10, len(all_splits))]])
-
-
-# In[26]:
+def delete_file_from_store(name: str) -> int:
+    ids_to_remove = []
+    for (id, doc) in vector_store.store.items():
+        if name == doc['metadata']['document_name']:
+            ids_to_remove.append(id)
+    vector_store.delete(ids_to_remove)
+    #print('File deleted:', name)
+    return len(ids_to_remove)
 
 
-print(get_meta_doc(extract))
+def inspect_vector_store(top_n: int=10) -> list:
+    docs = []
+    for index, (id, doc) in enumerate(vector_store.store.items()):
+        if index < top_n:
+            docs.append({
+                'id': id,
+                'document_name': doc['metadata']['document_name'],
+                'insert_date': doc['metadata']['insert_date'],
+                'text': doc['text']
+                })
+            # docs have keys 'id', 'vector', 'text', 'metadata'
+            # print(f"{id} {doc['metadata']['document_name']}: {doc['text']}")
+        else:
+            break
+    return docs
 
 
-# In[ ]:
+def get_vector_store_info():
+    nb_docs = 0
+    max_date, min_date = None, None
+    documents = set()
+    for (id, doc) in vector_store.store.items():
+        nb_docs += 1
+        if max_date is None or max_date < doc['metadata']['insert_date']:
+            max_date = doc['metadata']['insert_date']
+        if min_date is None or min_date > doc['metadata']['insert_date']:
+            min_date = doc['metadata']['insert_date']
+        documents.add(doc['metadata']['document_name'])
+    return {
+        'nb_chunks': nb_docs,
+        'min_insert_date': min_date,
+        'max_insert_date': max_date,
+        'nb_documents': len(documents)
+    }
 
 
+def retrieve(question: str):
+    """Retrieve documents similar to a question.
 
+    Args:
+        question (str): text of the question
+
+    Returns:
+        list[TODO]: list of similar documents retrieved from the vector store
+    """
+    retrieved_docs = vector_store.similarity_search(question)
+    return retrieved_docs
+
+
+def build_qa_messages(question: str, context: str) -> list[str]:
+    messages = [
+    (
+        "system",
+        "You are an assistant for question-answering tasks.",
+    ),
+    (
+        "system",
+        """Use the following pieces of retrieved context to answer the question.
+        If you don't know the answer, just say that you don't know.
+        Use three sentences maximum and keep the answer concise.
+        {}""".format(context),
+    ),
+    (  
+        "user",
+        question
+    ),]
+    return messages
+
+
+def answer_question(question: str) -> str:
+    """Answer a question by retrieving similar documents in the store.
+
+    Args:
+        question (str): text of the question
+
+    Returns:
+        str: text of the answer
+    """
+    inspect_vector_store()
+    docs = retrieve(question)
+    docs_content = "\n\n".join(doc.page_content for doc in docs)
+    print("Question:", question)
+    print("------")
+    for doc in docs:
+        print("Chunk:", doc.id)
+        print(doc.page_content)
+        print("------")
+    messages = build_qa_messages(question, docs_content)
+    response = llm.invoke(messages)
+    return response.content
 
